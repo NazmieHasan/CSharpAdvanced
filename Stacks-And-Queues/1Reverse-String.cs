cbdf43a20cedbfee629cca08edfdc79f1bfa17c3
NazmieHasan <Nazi87@abv.bg>
2023-01-27 18:55:59 +0200

    Initial commit

---
+++ Stacks-And-Queues/1Reverse-String.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharpAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+			
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var character in input)
+            {
+                stack.Push(character);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Stacks-And-Queues/4Matching-Brackets.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharpAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+          
+            Stack<int> openBracketIndexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openBracketIndexes.Push(i);
+                }
+              
+                if (expression[i] == ')')
+                {
+                    int openBracket = openBracketIndexes.Pop();
+
+                    for (int j = openBracket; j <= i; j++)
+                    {
+                        Console.Write(expression[j]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+			
+        }
+    }
+}
